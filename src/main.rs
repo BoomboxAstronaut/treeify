204f680aa77c7581e576d89456be1689545595b6
BoomboxAstronaut <88296897+BoomboxAstronaut@users.noreply.github.com>
2022-08-16 05:54:49 -0400

    Added tests, updated for test compliance

--- src/main.rs
+++ src/main.rs
@@ -12,28 +12,21 @@ struct LSubGroup {
     n_match: u8,
     i_next: u8,
     lim: u16,
-    eph: bool
+    eph: u8
 }
 
 
 impl LSubGroup {
 
     fn info(&self) {
-        println!("Info: LL: {:?}, Out: {:?}, Counts: {:?}, Match Count: {:?}, Next Index {:?}", 
+        println!("Info: LL: {:?}, Out: {:?}, Counts: {:?}, Match Count: {:?}, Next Index: {:?}, Current: {:?}", 
             char::from(self.l_last),
             String::from_utf8(self.outp.clone()).unwrap(),
             self.counts,
             self.n_match,
-            self.i_next
+            self.i_next,
+            String::from_utf8(self.inp[0].clone()).unwrap()
         );
-        let mut in_max = self.inp.len();
-        if  in_max > 5 {
-            in_max = 5;
-        }
-
-        for x in &self.inp[0..in_max] {
-            println!(": {:?}", String::from_utf8(x.clone()).unwrap())
-        }
     }
     
     fn decount(&mut self) {
@@ -61,11 +54,6 @@ impl LSubGroup {
         self.outp.extend([self.l_last, 40u8].iter());
         Self::zero(self);
     }
-    
-    fn clear_line(&mut self) {
-        self.outp.push(self.l_last);
-        self.outp.append(&mut self.inp.remove(0));
-    }
 
 }
 
@@ -90,20 +78,30 @@ fn extract_sub_group(master_list: &mut Vec<Vec<u8>>) -> LSubGroup {
         n_match: 0,
         i_next: 0,
         lim: 1024,
-        eph: false
+        eph: 0
     };
 
     return sub_group;
 }
 
 
-fn treeify(mut words: LSubGroup) -> Option<Vec<u8>> {
+fn treeify(mut words: LSubGroup, dbg: bool) -> Option<Vec<u8>> {
+
+    if words.inp.len() == 1 {
+        words.outp.pop();
+        words.outp.append(&mut words.inp[0]);
+        words.outp.push(124u8);
+        return Some(words.outp)
+    }
 
     while words.inp.len() > 0 {
 
-        //words.info();
-        
+        if dbg {
+            words.info();
+        }
+
         if words.inp.len() == 1 {
+            // Last Line
             words.outp.append(&mut words.inp.remove(0));
             for _ in 0..words.counts.len() {
                 words.outp.push(41u8);
@@ -111,10 +109,11 @@ fn treeify(mut words: LSubGroup) -> Option<Vec<u8>> {
             words.outp.push(124u8);
 
         } else if words.i_next == 0 {
+            // Index reset to 0
             if words.inp[0].len() == 0 {
                 words.inp.remove(0);
                 words.l_last = 0;
-                words.eph = true;
+                words.eph += 1;
                 words.i_next = 0;
                 words.decount();
             } else {
@@ -122,9 +121,9 @@ fn treeify(mut words: LSubGroup) -> Option<Vec<u8>> {
                 words.incr()
             }
         } else if words.l_last == words.inp[words.i_next as usize][0] {
+            // Letter Matched
             if &words.i_next >= words.counts.last().unwrap() {
-                words.clear_line();
-                words.decount();
+                words.outp.push(words.l_last);
                 words.zero();
             } else {
                 words.inp[words.i_next as usize].remove(0);
@@ -134,18 +133,20 @@ fn treeify(mut words: LSubGroup) -> Option<Vec<u8>> {
                 words.consecutive_match();
             }
         } else if words.l_last != words.inp[words.i_next as usize][0] {
+            // Letter not Matched
             if words.n_match > 1 {
                 words.consecutive_match();
             } else if words.n_match == 1 {
-                if words.inp[0].len() == 0 && words.eph && words.counts.last() == Some(&1) {
+                if words.inp[0].len() == 0 && words.eph > 0 && words.counts.last() == Some(&1) {
                     words.inp.remove(0);
                     words.outp.pop();
                     words.counts.pop();
                     words.outp.push(words.l_last);
                     words.outp.push(63u8);
-                    words.eph = false;
+                    words.eph -= 1;
                 } else {
-                   words.clear_line(); 
+                    words.outp.push(words.l_last);
+                    words.outp.append(&mut words.inp.remove(0));
                 }
                 if words.counts.last() > Some(&1u8) {
                     words.outp.push(124u8);
@@ -156,10 +157,11 @@ fn treeify(mut words: LSubGroup) -> Option<Vec<u8>> {
         }
 
         while words.counts.last() < Some(&1) {
+            // Clear Counts
             words.outp.push(41u8);
-            if words.eph {
+            if words.eph > 0 {
                 words.outp.push(63u8);
-                words.eph = false;
+                words.eph -= 1;
             }
             if words.counts.len() > 1 {
                 words.counts.reverse();
@@ -179,12 +181,15 @@ fn treeify(mut words: LSubGroup) -> Option<Vec<u8>> {
     return Some(words.outp)
 }
 
+fn get_file(file_name: String) -> io::Result<Vec<Vec<u8>>> {
 
-fn main() -> io::Result<()> {
+    let word_vectors: Vec<Vec<u8>> = io::Cursor::new(fs::read(&file_name)?).split(b'\n').map(|x| x.unwrap()).collect();
+    return Ok(word_vectors)
+}
 
-    let argv: Vec<String> = env::args().collect();
-    let mut wlist: Vec<Vec<u8>> = io::Cursor::new(fs::read(&argv[1])?).split(b'\n').map(|x| x.unwrap()).collect();
+fn parse_file(file: String, dbg: bool) -> Vec<u8> {
 
+    let mut wlist: Vec<Vec<u8>> = get_file(file).unwrap();
     let mut word_tree: Vec<u8> = Vec::new();
     let mut letter_group: LSubGroup;
     let mut sub_tree: Vec<u8>;
@@ -192,14 +197,99 @@ fn main() -> io::Result<()> {
     while wlist.len() > 0 {
         
         letter_group = extract_sub_group(&mut wlist);
-        sub_tree = treeify(letter_group).unwrap();
+        sub_tree = treeify(letter_group, dbg).unwrap();
         word_tree.append(&mut sub_tree);
         
     }
 
-    println!("{}", String::from_utf8(word_tree.clone()).unwrap());
-    Ok(())
+    word_tree.pop();
+    return word_tree
 }
+
+fn main() -> io::Result<()> {
+
+    let argv: Vec<String> = env::args().collect();
+    let output: Vec<u8> = parse_file(argv[1].clone(), false);
+
+    println!("{}", String::from_utf8(output.clone()).unwrap());
+    Ok(())
+
+}
+
+
+
+
+#[cfg(test)]
+mod tests {
+    use crate::parse_file;
+
+    
+    #[test]
+    fn overall_1() {
+        let locus: String = String::from("tests/tfile1");
+        assert_eq!(
+            "A(aron|b(dullah|igail)|dam|hmed|l(an|bert|e(ssandro|x(ander|is))|i(ce)?|ma)|m(anda|ber|elia|y)|n(astasia|dre(a|w)|gela|na?|t(hony|oni))|rthur|shley|u(rora|stin)|va)",
+            String::from_utf8(parse_file(locus, false).clone()).unwrap()
+        );        
+    }
+
+    #[test]
+    fn overall_2() {
+
+        let locus: String = String::from("tests/tfile2");
+        assert_eq!(
+            "S(a(m(antha|uel)|ndra|rah?)|cott|e(an|rgei)|h(aron|irley)|o(f(ia|ía)|phia)|te(ph(anie|en)|ven)|usan)",
+            String::from_utf8(parse_file(locus, false).clone()).unwrap()
+        );
+    }
+    
+    #[test]
+    fn overall_3() {
+
+        let locus: String = String::from("tests/tfile3");
+        assert_eq!(
+            "M(a(dison|hmoud|r(garet|i(a|e|lyn)|k|t(ha|ina|ín)|y(am)?|ía)|son|t(eo|t(eo|hew))|xim)|e(gan|lissa)|i(ch(ael|elle)|khail)|ohamed|ustafa)",
+            String::from_utf8(parse_file(locus, false).clone()).unwrap()
+        );
+    }
+    
+    #[test]
+    fn overall_4() {
+
+        let locus: String = String::from("tests/tfile4");
+        assert_eq!(
+            "L(eon(ardo)?|i(am|nda|sa)|o(gan|r(enzo|i)|uise?)|ucía|yn|éo)",
+            String::from_utf8(parse_file(locus, false).clone()).unwrap()
+        );
+    }
+
+    #[test]
+    fn overall_5() {
+
+        let locus: String = String::from("tests/tfile5");
+        assert_eq!(
+            "a(bc(d(dd)?|e|fff)?|zzz)",
+            String::from_utf8(parse_file(locus, false).clone()).unwrap()
+        );
+    }
+
+    #[test]
+    fn overall_6() {
+
+        let locus: String = String::from("tests/tfile6");
+        assert_eq!(
+            "abc|bcd|efg",
+            String::from_utf8(parse_file(locus, false).clone()).unwrap()
+        );
+    }
+
+}
+
+
+
+
+
+
 
 
 
